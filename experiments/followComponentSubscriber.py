9ec58cd0bbe98d0b7f3bbdcc220b570ff0d7100a
Roberto Arista <arista.rob@gmail.com>
2022-01-28 15:30:05 +0100

    warning

--- experiments/followComponentSubscriber.py
+++ experiments/followComponentSubscriber.py
@@ -4,6 +4,12 @@
 # Get a notification when a component inside a glyph changes #
 # ---------------------------------------------------------- #
 
+"""
+This snippet should work, but there is an issue in Subscriber
+It's safer to use a .reversedComponentMapping() method inside a
+glyph changed callback, we'll use that in Longboard
+"""
+
 # -- Modules -- #
 from mojo.subscriber import Subscriber, registerCurrentFontSubscriber
 
